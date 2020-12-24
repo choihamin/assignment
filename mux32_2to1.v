9ed40c6a6df00120aea1f8ea2ec20f305785a21f
choihamin <47838599+choihamin@users.noreply.github.com>
2020-12-24 15:17:04 +0900

    Add files via upload

---
+++ mux32_2to1.v
@@ -0,0 +1,14 @@
+module mux32_2to1 (out, in0, in1, sel);
+	output reg  [31:0] out;
+	input [31:0] in0;
+	input [31:0] in1;
+	input sel;
+
+  	always @(sel)
+    begin
+      if (sel == 0)
+			out = in0;
+      else
+			out = in1;
+    end
+endmodule 
